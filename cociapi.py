6f815aa4bda223d69bd5254883f2c67c7306ce8c
Silvio Peroni <essepuntato@gmail.com>
2018-05-25 13:45:33 +0200

    Upload COCI API new

--- cociapi.py
+++ cociapi.py
@@ -15,8 +15,27 @@
 # SOFTWARE.
 
 __author__ = 'essepuntato'
+from urllib.parse import quote, unquote
 
 
 def lower(s):
-    return s,
-    #return s.lower(),
+    return s.lower(),
+
+
+def encode(s):
+    return quote(s),
+
+
+def decode_doi(res, *args):
+    header = res[0]
+    field_idx = []
+
+    for field in args:
+        field_idx.append(header.index(field))
+
+    for row in res[1:]:
+        for idx in field_idx:
+            t, v = row[idx]
+            row[idx] = t, unquote(v)
+
+    return res
