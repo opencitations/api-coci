bedbd2a31ccd337fbebfd6ce7f5f18e7cd72e5b4
Silvio Peroni <essepuntato@gmail.com>
2018-08-21 00:16:47 +0200

    New versio of API with OA link

--- cociapi.py
+++ cociapi.py
@@ -19,7 +19,7 @@ from urllib.parse import quote, unquote
 from requests import get
 from rdflib import Graph, URIRef
 from rdflib.namespace import RDF, OWL, Namespace
-from re import sub
+from json import loads
 
 
 def lower(s):
@@ -50,9 +50,6 @@ def split_dois(s):
 
 
 def metadata(res, *args):
-    base_api_url = "https://doi.org/"
-    rdf_format = "application/rdf+xml"
-
     # doi, reference, citation_count
     header = res[0]
     doi_field = header.index("doi")
@@ -63,104 +60,148 @@ def metadata(res, *args):
     for row in res[1:]:
         citing_doi = row[doi_field][1]
 
-        try:
-            r = get(base_api_url + citing_doi,
-                    headers={"Accept": rdf_format,
-                             "User-Agent": "COCI REST API (via OpenCitations - "
-                                           "http://opencitations.net; mailto:contact@opencitations.net)"}, timeout=30)
-            if r.status_code == 200:
-                g = Graph()
-                g.parse(data=r.text, format=rdf_format)
-                __add_data(row, g, additional_fields)
-            else:
-                row.extend([""] * len(additional_fields))  # empty list
-        except Exception as e:
-            raise e
+        r = None
+        for p in (__crossref_parser, __datacite_parser):
+            if r is None:
+                r = p(citing_doi)
+
+        if r is None:
             row.extend([""] * len(additional_fields))  # empty list
+        else:
+            row.extend(r)
 
     return res
 
 
-def __add_data(row, g, additional_fields):
-    res = None
-    if len(list(g.triples((None, URIRef("http://prismstandard.org/namespaces/basic/2.1/doi"), None)))):
-        res = __crossref_parser(g)
-    elif len(list(g.triples((None, RDF.type, URIRef("http://schema.org/ScholarlyArticle"))))):
-        res = __datacite_parser(g)
+def __crossref_parser(doi):
+    api = "https://api.crossref.org/works/%s"
 
-    if res is None or len(res) == 0:
-        row.extend([""] * len(additional_fields))  # empty list
-    else:
-        row.extend([str(item) if item is not None else "" for item in res[0]])
+    try:
+        r = get(api % doi,
+                headers={"User-Agent": "COCI REST API (via OpenCitations - "
+                                       "http://opencitations.net; mailto:contact@opencitations.net)"}, timeout=30)
+        if r.status_code == 200:
+            json_res = loads(r.text)
+            if "message" in json_res:
+                body = json_res["message"]
+
+                authors = []
+                if "author" in body:
+                    for author in body["author"]:
+                        author_string = None
+                        if "family" in author:
+                            author_string = author["family"]
+                            if "given" in author:
+                                author_string += ", " + author["given"]
+                                if "ORCID" in author:
+                                    author_string += ", " + author["ORCID"]
+                        if author_string is not None:
+                            authors.append(author_string)
+
+                year = ""
+                if "issued" in body and "date-parts" in body["issued"] and len(body["issued"]["date-parts"]) and \
+                        len(body["issued"]["date-parts"][0]):
+                    year = str(body["issued"]["date-parts"][0][0])
+
+                title = ""
+                if "title" in body:
+                    title = body["title"][0]
+
+                source_title = ""
+                if "container-title" in body:
+                    source_title = body["container-title"][0]
+
+                volume = ""
+                if "volume" in body:
+                    volume = body["volume"]
+
+                issue = ""
+                if "issue" in body:
+                    issue = body["issue"]
+
+                page = ""
+                if "page" in body:
+                    page = body["page"]
+
+                return ["; ".join(authors), year, title, source_title, volume, issue, page]
+
+    except Exception as e:
+        pass  # do nothing
 
 
-def __crossref_parser(g):
-    return list(g.query("""
-        PREFIX foaf: <http://xmlns.com/foaf/0.1/>
-        PREFIX dcterms: <http://purl.org/dc/terms/>
-        PREFIX prism: <http://prismstandard.org/namespaces/basic/2.1/>
-        PREFIX owl: <http://www.w3.org/2002/07/owl#>
-        SELECT DISTINCT ?author ?year ?title ?source_title ?volume ?issue ?page
-        WHERE {
-            ?article prism:doi ?string_doi .
-            {
-                SELECT ?article (GROUP_CONCAT(?name; separator="; ") as ?author) {
-                    {
-                        SELECT ?article ?fn ?gn ?orcid {
-                            ?article dcterms:creator ?a .
-                            ?a foaf:familyName ?fn ;
-                                foaf:givenName ?gn .
-                            OPTIONAL {
-                                ?a owl:sameAs ?another_a .
-                                BIND(STRAFTER(str(?another_a), "http://orcid.org/") as ?orcid)   
-                            }
-                        } ORDER BY ?article ?fn ?gn
+def __datacite_parser(doi):
+    api = "https://doi.org/"
+    rdf_format = "application/rdf+xml"
+
+    try:
+        r = get(api + doi,
+                headers={"Accept": rdf_format,
+                         "User-Agent": "COCI REST API (via OpenCitations - "
+                                       "http://opencitations.net; mailto:contact@opencitations.net)"}, timeout=30)
+        if r.status_code == 200:
+            g = Graph()
+            g.parse(data=r.text, format=rdf_format)
+            res = list(g.query("""
+                PREFIX schema: <http://schema.org/>
+                SELECT DISTINCT ?author ?year ?title ?source_title ?volume ?issue ?page
+                WHERE {
+                    ?article a schema:ScholarlyArticle .
+                    
+                    {   
+                        SELECT ?article (GROUP_CONCAT(?name; separator="; ") as ?author) {
+                            {
+                                SELECT ?article ?fn ?gn ?orcid {
+                                    ?article schema:author ?a .
+                                    ?a schema:familyName ?fn ;
+                                        schema:givenName ?gn .
+                                    BIND(STRAFTER(str(?a), "https://orcid.org/") as ?orcid)
+                                    BIND(CONCAT(?fn, ", ", ?gn, IF(bound(?orcid), CONCAT(", ", ?orcid), "")) as ?name)
+                                } ORDER BY ?article ?fn ?gn
+                            } 
+                            BIND(CONCAT(?fn, ", ", ?gn, IF(bound(?orcid), CONCAT(", ", ?orcid), "")) as ?name)
+                        } GROUP BY ?article
                     }
                     
-                    BIND(CONCAT(?fn, ", ", ?gn, IF(bound(?orcid), CONCAT(", ", ?orcid), "")) as ?name)
-                } GROUP BY ?article
-            }
-            OPTIONAL { 
-                ?article dcterms:date ?date .
-                BIND(SUBSTR(str(?date), 1, 4) as ?year)
-            }
-            OPTIONAL { ?article dcterms:title ?title }
-            OPTIONAL { ?article dcterms:isPartOf / dcterms:title ?source_title }
-            OPTIONAL { ?article prism:volume ?volume }
-            OPTIONAL { ?article prism:issueIdentifier ?issue }
-            OPTIONAL {
-                ?article prism:startingPage ?sp .
-                OPTIONAL { ?article prism:endingPage ?ep }
-                BIND(CONCAT(?sp, IF(bound(?ep), CONCAT("-", ?ep), "")) as ?page)
-            }
-        }"""))
+                    OPTIONAL { ?article schema:name ?title }
+                    OPTIONAL { 
+                        ?article schema:datePublished ?date .
+                        BIND(SUBSTR(str(?date), 1, 4) as ?year)
+                    }
+                    OPTIONAL { ?article schema:publisher/schema:name ?source_title }
+                }"""))[0]
+
+            return [str(item) if item is not None else "" for item in res]
+    except Exception as e:
+        pass  # no nothing
 
 
-def __datacite_parser(g):
-    return list(g.query("""
-        PREFIX schema: <http://schema.org/>
-        SELECT DISTINCT ?author ?year ?title ?source_title ?volume ?issue ?page
-        WHERE {
-            ?article a schema:ScholarlyArticle .
-            
-            {   
-                SELECT ?article (GROUP_CONCAT(?name; separator="; ") as ?author) {
-                    {
-                        SELECT ?article ?fn ?gn ?orcid {
-                            ?article schema:author ?a .
-                            ?a schema:familyName ?fn ;
-                                schema:givenName ?gn .
-                            BIND(STRAFTER(str(?a), "https://orcid.org/") as ?orcid)
-                            BIND(CONCAT(?fn, ", ", ?gn, IF(bound(?orcid), CONCAT(", ", ?orcid), "")) as ?name)
-                        } ORDER BY ?article ?fn ?gn
-                    } 
-                    BIND(CONCAT(?fn, ", ", ?gn, IF(bound(?orcid), CONCAT(", ", ?orcid), "")) as ?name)
-                } GROUP BY ?article
-            }
-            
-            OPTIONAL { ?article schema:name ?title }
-            OPTIONAL { 
-                ?article schema:datePublished ?date .
-                BIND(SUBSTR(str(?date), 1, 4) as ?year)
-            }
-        }"""))
+def oalink(res, *args):
+    base_api_url = "https://api.unpaywall.org/v2/%s?email=contact@opencitations.net"
+
+    # doi, reference, citation_count
+    header = res[0]
+    doi_field = header.index("doi")
+    additional_fields = ["oa_link"]
+
+    header.extend(additional_fields)
+
+    for row in res[1:]:
+        citing_doi = row[doi_field][1]
+
+        try:
+            r = get(base_api_url % citing_doi,
+                    headers={"User-Agent": "COCI REST API (via OpenCitations - "
+                                           "http://opencitations.net; mailto:contact@opencitations.net)"}, timeout=30)
+            if r.status_code == 200:
+                res_json = loads(r.text)
+                if "best_oa_location" in res_json and res_json["best_oa_location"] is not None and \
+                        "url" in res_json["best_oa_location"]:
+                    row.append(res_json["best_oa_location"]["url"])
+                else:
+                    row.append("")  # empty element
+            else:
+                row.append("")  # empty element
+        except Exception as e:
+            row.append("")  # empty element
+
+    return res
